ce951f5c439b7361d63793160e2914d05431c880
dowlinga <dowlinga@mail.gvsu.edu>
2016-09-21 00:17:21 -0400

    Update CounterModel.java

--- src/checkstylepack/CounterModel.java
+++ src/checkstylepack/CounterModel.java
@@ -3,26 +3,41 @@ package checkstylepack;
 public class CounterModel implements IModel {
 	
 	private int data;
-
+	
+	/**
+	 * default constructor for CounterModel class
+	 */
 	public CounterModel() {
 		data = 0;
 	}
 	
+	/**
+	 * getter that returns the data from CounterModel
+	 */
 	@Override
 	public final int getValue() {
 		return data;
 	}
-
+	
+	/**
+	 * add method that adds the @param "val" to the current data
+	 */
 	@Override
 	public final void add(final int val) {
 		data += val;
 	}
-
+	
+	/**
+	 * subtract method that subtracts "val" from data
+	 */
 	@Override
 	public final void sub(final int val) {
 		data -= val;
 	}
-
+	
+	/**
+	 * method that resets data to 0
+	 */
 	@Override
 	public final void reset() {
 		data = 0;
